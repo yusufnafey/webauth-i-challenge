b90687293f841f0486984cdafbcf64bbb9d5cc33
Yusuf Nafey <yusufnafey@yahoo.com>
2019-08-19 23:17:38 -0700

    implements successful post login endpoint

--- server.js
+++ server.js
@@ -30,7 +30,22 @@ server.post("/api/register", (req, res) => {
   }
 });
 
-server.post("/api/login", (req, res) => {});
+server.post("/api/login", (req, res) => {
+  const { username } = req.body;
+
+  UsersModel.findBy({ username })
+    .first()
+    .then(user => {
+      if (user) {
+        res.status(200).json({ message: "Logged in." });
+      } else {
+        res.status(401).json({ message: "You shall not pass!" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: "There was an error logging in." });
+    });
+});
 
 server.get("/api/users", (req, res) => {
   UsersModel.find()
--- users-model.js
+++ users-model.js
@@ -3,18 +3,17 @@ const db = require("./db-config");
 module.exports = {
   find,
   findBy,
-  add,
-  findById
+  add
 };
 
 function find() {
   return db("users").select("id", "username", "password");
 }
 
-function findBy(filter) {}
+function findBy(filter) {
+  return db("users").where(filter);
+}
 
 function add(user) {
   return db("users").insert(user);
 }
-
-function findById(id) {}
