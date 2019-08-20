d93dbda941b130ea84daec5a040e4c2ebd288fc6
Yusuf Nafey <yusufnafey@yahoo.com>
2019-08-20 13:15:42 -0700

    implements logout

--- server.js
+++ server.js
@@ -11,10 +11,29 @@ const knexConnection = require("./db-config");
 server.use(express.json());
 server.use(session(sessionOptions));
 
+// =========== SESSION OPTIONS ===========
+const sessionOptions = {
+  name: "testsession",
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    secure: process.env.COOKIE_SECURE || false,
+    maxAge: 1000 * 60 * 60,
+    httpOnly: true
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new KnexSessionStore({
+    knex: knexConnection,
+    createtable: true,
+    clearInterval: 1000 * 60 * 60
+  })
+};
+
 server.get("/", (req, res) => {
   res.send("Hello from /");
 });
 
+// =========== REGISTER ===========
 server.post("/api/register", (req, res) => {
   const user = req.body;
   const hash = bcrypt.hashSync(user.password, 12);
@@ -38,6 +57,7 @@ server.post("/api/register", (req, res) => {
   }
 });
 
+// =========== LOG IN ===========
 server.post("/api/login", (req, res) => {
   const { username, password } = req.body;
 
@@ -45,6 +65,8 @@ server.post("/api/login", (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.username = user.username;
+        req.session.loggedIn = true;
         res.status(200).json({ message: "Logged in." });
       } else {
         res.status(401).json({ message: "You shall not pass!" });
@@ -55,6 +77,7 @@ server.post("/api/login", (req, res) => {
     });
 });
 
+// =========== GET USERS ===========
 server.get("/api/users", restricted, (req, res) => {
   UsersModel.find()
     .then(users => {
@@ -65,6 +88,13 @@ server.get("/api/users", restricted, (req, res) => {
         .status(500)
         .json({ message: "There was an error getting the users." });
     });
+});
+
+// =========== LOG OUT ===========
+server.get("/logout", (req, res) => {
+  req.session.destroy(() => {
+    res.status(200).json({ bye: user.username });
+  });
 });
 
 // =========== RESTRICTED FUNCTION ===========
