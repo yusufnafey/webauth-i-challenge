884ef52cc59cea4af0f2ad6cd7520b3ef7f29cc6
Yusuf Nafey <yusufnafey@yahoo.com>
2019-08-20 12:32:24 -0700

    finishes day 1 mvp

--- server.js
+++ server.js
@@ -45,12 +45,12 @@ server.post("/api/register", (req, res) => {
 });
 
 server.post("/api/login", (req, res) => {
-  const { username } = req.body;
+  const { username, password } = req.body;
 
   UsersModel.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
+      if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: "Logged in." });
       } else {
         res.status(401).json({ message: "You shall not pass!" });
